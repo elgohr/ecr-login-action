4c4f87eed806994c6eb8e3d24fa77f9754498a4d
Lars Gohr <larsgohr@posteo.de>
2019-09-21 21:25:22 +0200

    Use default username if the username could not be found

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,12 @@ connectionString=$(aws ecr get-login)
 cleanedConnection=$(echo $connectionString | sed -e "s/docker login -u //g" | sed -e "s/ -p//g" | sed -e "s/ -e none//g")
 connectionParts=($cleanedConnection)
 
-echo ::set-output name=username::"${connectionParts[0]}"
+if [ -z "${connectionParts[0]}" ]; then
+  USERNAME="AWS"
+else
+  USERNAME="${connectionParts[0]}"
+fi
+
+echo ::set-output name=username::"${USERNAME}"
 echo ::set-output name=password::"${connectionParts[1]}"
 echo ::set-output name=registry::"${connectionParts[2]}"
