61530553dbceb962211c7424b0a7c9522231a8e7
David Dahl <david@struqtur.se>
2019-10-01 14:58:14 +0200

    Use export for aws auth

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-aws configure <<-EOF
-$INPUT_ACCESS_KEY
-$INPUT_SECRET_ACCESS_KEY
-$INPUT_REGION
-text
-EOF
+export AWS_ACCESS_KEY_ID=$INPUT_ACCESS_KEY
+export AWS_SECRET_ACCESS_KEY=$INPUT_SECRET_ACCESS_KEY
+export AWS_DEFAULT_REGION=$INPUT_REGION
 
 authTokenOutput=$(aws ecr get-authorization-token)
 authString=$(echo "$authTokenOutput" | jq -r '.authorizationData[].authorizationToken' | base64 -d)
